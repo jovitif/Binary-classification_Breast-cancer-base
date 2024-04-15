328cc62e465fdf89e379c5c048d2825ddc080866
João Sales <joaosales911@gmail.com>
2024-04-14 23:40:10 -0300

    feat: initializing project

---
+++ breast_cancer_simples.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+previsores = pd.read_csv('entradas_breast.csv')
+classe = pd.read_csv('saidas_breast.csv')
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
+
